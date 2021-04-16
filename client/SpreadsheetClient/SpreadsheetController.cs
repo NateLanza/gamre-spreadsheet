59506cd6085820763e5d73e84210b9e9b714d289
Nat C <dolphinpower900@gmail.com>
2021-04-15 23:01:30 -0600

    Finished initial connection protocol

--- client/SpreadsheetClient/SpreadsheetController.cs
+++ client/SpreadsheetClient/SpreadsheetController.cs
@@ -87,9 +87,10 @@ namespace SS {
         private SocketState Connection;
 
         /// <summary>
-        /// Whether this client is connected to a server
+        /// Current state of the server connection.
+        /// Defined by a series of constants in the class ConnectionState
         /// </summary>
-        public bool Connected { get; private set; }
+        public int ConnectionState { get; private set; }
 
         /// <summary>
         /// ID of this client, will be -1 if not connected
@@ -102,7 +103,7 @@ namespace SS {
         /// <param name="state">State object of current spreadsheet</param>
         public SpreadsheetController(SpreadsheetState state) {
             this.State = state;
-            Connected = false;
+            ConnectionState = ConnectionStates.Disconnected;
             ID = -1;
         }
 
@@ -126,7 +127,7 @@ namespace SS {
             if (connection.ErrorOccured) {
                 ConnectionAttempted(true, null);
             } else {
-                Connected = true;
+                ConnectionState = ConnectionStates.WaitForSpreadsheets;
                 Connection = connection;
                 Connection.OnNetworkAction = SpreadsheetListCallback;
                 Networking.GetData(Connection);
@@ -139,7 +140,7 @@ namespace SS {
         /// <param name="ss">Connection to server</param>
         private void SpreadsheetListCallback(SocketState ss) {
             // Ensure we have received a complete list
-            if (!ss.GetData().Contains("\n\n")) {
+            if (!ss.GetData().EndsWith("\n\n")) {
                 Networking.GetData(ss);
                 return;
             }
@@ -149,6 +150,7 @@ namespace SS {
             ss.RemoveData(0, serverData.Length);
             List<string> spreadsheets = new List<string>(serverData.Split('\n'));
             ConnectionAttempted(false, spreadsheets);
+            ConnectionState = ConnectionStates.WaitForSpreadsheetConnection;
         }
 
         /// <summary>
@@ -156,14 +158,102 @@ namespace SS {
         /// Should only be called after a connection to server has been established
         /// and a list of spreadsheets has been received.
         /// </summary>
-        /// <exception cref="InvalidOperationException">If a connection to the server has not been established</exception>
-        /// <param name="name"></param>
+        /// <exception cref="InvalidOperationException">If ConnectionState != ConnectionStates.WaitForSpreadsheetConnection</exception>
+        /// <exception cref="ArgumentException">If name contains \n</exception>
+        /// <param name="name">Name of spreadsheet to connect to</param>
         public void ConnectToSpreadsheet(string name) {
+            // Validate input
+            if (ConnectionState != ConnectionStates.WaitForSpreadsheetConnection)
+                throw new InvalidOperationException("Cannot connect to a spreadsheet when the connection state is not WaitForSpreadsheetConnection");
+            if (name.Contains("\n"))
+                throw new ArgumentException("Param name cannot contain a newline character \\n");
+
+            // Send connection message to server
+            Networking.Send(Connection.TheSocket, name + "\n");
+            ConnectionState = ConnectionStates.WaitForID;
+            Connection.OnNetworkAction = WaitForIDCallback;
+            Networking.GetData(Connection);
+        }
+
+        /// <summary>
+        /// Networking callback while receiving data leading up to and including
+        /// this client's ID. Used right after sending a spreadsheet name to the server
+        /// </summary>
+        /// <param name="ss">Connection</param>
+        private void WaitForIDCallback(SocketState ss) {
+            // Get data, make sure we have a complete token
+            string serverData = ss.GetData();
+            if (!serverData.Contains("\n")) {
+                Networking.GetData(ss);
+                return;
+            }
+
+            // Parse tokens
+            List<string> serverTokens = new List<string>(serverData.Split('\n'));
+            ss.RemoveData(0, serverData.LastIndexOf('\n'));
+
+            // Remove incomplete token
+            if (!serverData.EndsWith("\n"))
+                serverTokens.RemoveAt(serverTokens.Count - 1);
+
+            bool receivedID = false;
+            // See if we've received the ID, set if so
+            if (!serverTokens[serverTokens.Count - 1].Contains("{")) {
+                string ID = serverTokens[serverTokens.Count - 1];
+                if (int.TryParse(ID, out int intID)) {
+                    this.ID = intID;
+                    receivedID = true;
+                }
+                serverTokens.RemoveAt(serverTokens.Count - 1);
+            }
+
+            // Process json
+            foreach (string token in serverTokens)
+                ProcessServerJson(token);
+
+            // Move to next connection stage if ID received
+            if (receivedID) {
+                ConnectionState = ConnectionStates.Connected;
+                Connection.OnNetworkAction = ReceiveLoop;
+                IDReceived(ID);
+            }
+
+            // Get more data from server
+            Networking.GetData(Connection);
+        }
+
+        /// <summary>
+        /// Loop to receive information from the server
+        /// </summary>
+        /// <param name="ss">Connection</param>
+        private void ReceiveLoop(SocketState ss) {
+
+            Networking.GetData(ss);
+        }
+
+        /// <summary>
+        /// Processes a JSON message received from the server and triggers
+        /// all necessary events
+        /// </summary>
+        /// <param name="json"></param>
+        private void ProcessServerJson(string json) {
+
         }
 
         public void SendEditRequest(string cell, string contents) {
-            if (!Connected || ID == -1 || Connection.ErrorOccured)
-                return;
+            
+        }
+
+        public void SendRevertRequest(string cell) {
+
+        }
+
+        public void SendSelectRequest(string cell) {
+
+        }
+
+        public void SendUndoRequest() {
+
         }
     }
 }
