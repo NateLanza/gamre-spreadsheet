b16bb7fbcc8d69daf219cc4e8c812df48108093b
Nat C <dolphinpower900@gmail.com>
2021-04-24 20:24:29 -0600

    Implemented cell.cpp

--- Spreadsheet Server/Spreadsheet Server/Cell.cpp
+++ Spreadsheet Server/Spreadsheet Server/Cell.cpp
@@ -4,3 +4,15 @@
 
 Cell::Cell(const string name, const Formula contents) : name(name), contents(contents.ToString()) {
 }
+
+const string Cell::GetName() const {
+	return name;
+}
+
+const string Cell::GetContents() const {
+	return contents.ToString();
+}
+
+void Cell::SetContents(const Formula newContents) {
+	contents = newContents;
+}
