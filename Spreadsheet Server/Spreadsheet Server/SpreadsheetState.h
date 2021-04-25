23bb1a29fb022e977cec58b5974a5f654e6f4098
Nat C <dolphinpower900@gmail.com>
2021-04-24 18:12:04 -0600

    Changes to storage.h and SpreadsheetState.h

--- Spreadsheet Server/Spreadsheet Server/SpreadsheetState.h
+++ Spreadsheet Server/Spreadsheet Server/SpreadsheetState.h
@@ -2,6 +2,7 @@
 #include <list>
 #include <map>
 #include <set>
+#include <stack>
 #include <windows.data.json.h>
 #include <windows.networking.sockets.h>
 
@@ -15,15 +16,69 @@ using namespace std;
 #ifndef SpreadsheetState_H
 #define SpreadsheetState_H
 
+/// <summary>
+/// Represents the state of a currently open spreadsheet
+/// </summary>
 class SpreadsheetState
 {
+
+private:
+	/// <summary>
+	/// All cells in this spreadsheet
+	/// </summary>
+	set<Cell> spreadsheetCells;
+
+	/// <summary>
+	/// All edits made to this spreadsheet, in order of recency.
+	/// </summary>
+	stack<CellEdit> edits;
+
 public:
+	/// <summary>
+	/// Creates a new, blank spreadsheet
+	/// </summary>
 	SpreadsheetState();
 
-	set<Cell> spreadsheetCells;         // set for cells because the cells are unique
-	list<EditRequest> clientRequests;   // list so that we can pop the 'first' element as the first is the first edit that should be handled
-	list<CellEdit> cellUpdates;         // list of cells that need to be updated in spreadsheetCells
-	//map<int, socket> clients;         // have a map of client IDs and clients (need to know the socket setup for this)
+	/// <summary>
+	/// Creates a spreadsheet from a set of cells
+	/// Cell set is usually provided by the storage class, retreiving from a file
+	/// </summary>
+	/// <param name="cells">Cells to initialize into spreadsheets</param>
+	SpreadsheetState(set<Cell> cells, stack<CellEdit> edits);
+
+	/// <summary>
+	/// Edits the content of a cell and adds the edit to the edit stack
+	/// </summary>
+	/// <param name="name">Cell name</param>
+	/// <param name="content">New content</param>
+	/// <returns>True if cell contents were edited successfully,
+	/// false if content format was invalid or the edit would create a circular dependency</returns>
+	bool EditCell(string name, string content);
+
+	/// <summary>
+	/// Reverts most recent change to a certain cell and adds the revert to the edit stack
+	/// </summary>
+	/// <param name="cell">Cell to revert</param>
+	/// <returns>True if revert successfull, false if revert would create a circular dependency</returns>
+	bool RevertCell(string cell);
+
+	/// <summary>
+	/// Undoes the last edit to the spreadsheet
+	/// </summary>
+	/// <returns>True if edit undone, false if edit would create a circular dependency</returns>
+	bool UndoLastEdit();
+
+	/// <summary>
+	/// Returns all edits made to this spreadsheet as a stack, with most recent at the top
+	/// </summary>
+	/// <returns>Stack of edits</returns>
+	stack<CellEdit> GetEditHistory();
+
+	/// <summary>
+	/// Gets all non-empty cells in this spreadsheet
+	/// </summary>
+	/// <returns>Cells, as a list</returns>
+	list<Cell> GetPopulatedCells();
 
 };
 
--- Spreadsheet Server/Spreadsheet Server/Storage.h
+++ Spreadsheet Server/Spreadsheet Server/Storage.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <list>
 #include <map>
+#include <stack>
 #include "Cell.h"
 #include "SpreadsheetState.h"
 
@@ -10,19 +11,55 @@ using namespace std;
 #ifndef Storage_H
 #define Storage_H
 
+/// <summary>
+/// Simple wrapper struct for the list of Cells and stack of CellEdits 
+/// necessary to represent a spreadsheet
+/// </summary>
+struct StoredSpreadsheet {
+public:
+	/// <summary>
+	/// Cells in the spreadsheet
+	/// </summary>
+	set<Cell> cells;
+	/// <summary>
+	/// Edit history
+	/// </summary>
+	stack<CellEdit> edits;
+
+	/// <summary>
+	/// Creates a new StoredSpreadsheet from cells & edits
+	/// </summary>
+	/// <param name="cells">Non-empty cells in the spreadsheet</param>
+	/// <param name="edits">Edit history; most recent edit at the top</param>
+	StoredSpreadsheet(set<Cell> cells, stack<CellEdit> edits);
+};
+
+/// <summary>
+/// Handles storing of spreadsheets to files
+/// </summary>
 class Storage
 {
-private:
-	map <string, set<Cell>> spreadsheets;
 public:
+	/// <summary>
+	/// Opens a spreadsheet from file if it exists. 
+	/// If not, returns an empty list of cells & empty stack of CellEdits
+	/// </summary>
+	/// <param name="spreadsheetName">Spreadsheet to open (provided by client)</param>
+	/// <returns>Spreadsheet data</returns>
+	StoredSpreadsheet Open(string spreadsheetName);
 
-	set<Cell> Open(string spreadsheetName);		// returns cells so that the spreadsheet may be 'opened' ie. the cells can be sent to the client
-	void Save(string spreadsheetName, set<Cell> cellsInSpreadsheet);
-	void AddSpreadsheet(string name, set<Cell> newSpreadsheet);
-	void RemoveSpreadsheet(string name);
+	/// <summary>
+	/// Saves a spreadsheet to file
+	/// </summary>
+	/// <param name="spreadsheetName">Filename</param>
+	/// <param name="ss">Spreadsheet data</param>
+	void Save(string spreadsheetName, StoredSpreadsheet ss);
 
-	map<string, set<Cell>> GetSpreadsheets();
-	set<Cell> GetSpreadsheet(string key);
+	/// <summary>
+	/// Gets all spreadsheets on file
+	/// </summary>
+	/// <returns>A list of names of files which can be opened</returns>
+	list<string> GetSavedSpreadsheetNames();
 };
 
 #endif
