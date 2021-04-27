75f89ae99415f13cb50e47052f42f12bdd2089a7
u1190911@eng.utah.edu <u1190911@eng.utah.edu>
2021-04-26 21:24:54 -0600

    Added connection cpp file

---
+++ Spreadsheet Server/Spreadsheet Server/Connection.cpp
@@ -0,0 +1,12 @@
+//#include "Connection.h"
+//
+//Connection::Connection(const Connection& c)
+//{
+//	*this = c;
+//}
+//
+//
+//Connection& Connection::operator=(const Connection* c) 
+//{
+//
+//}
--- Spreadsheet Server/Spreadsheet Server/Connection.h
+++ Spreadsheet Server/Spreadsheet Server/Connection.h
@@ -13,14 +13,11 @@
 struct Connection {
 	boost::asio::ip::tcp::socket socket;				// The socket
 	boost::asio::streambuf read_buffer;					// The data received 
-	int ID;
-
+	
 	Connection(boost::asio::io_service& io_service)		// Creates a Connection with io_service, which facilitates ansynchrony. 
 		: socket(io_service), read_buffer() { }
 
-	Connection(boost::asio::io_service& io_service, int ID)		// Creates a Connection with io_service, which facilitates ansynchrony. 
-		: socket(io_service), read_buffer(), ID(ID) { }
-
+	
 	Connection(boost::asio::io_service& io_service, size_t max_buffer_size) // Creates the connection with an additional buffer_size, if specified.
 		: socket(io_service), read_buffer(max_buffer_size) { }
 
