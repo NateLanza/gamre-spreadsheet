ad369ee0266e8feec28ea3a4ca9d238fdb45ac89
u1262028 <u1262028@eng.utah.edu>
2021-04-27 19:49:15 -0600

    GhostClient connection finished. Test number one (basic connection test) implemented.

--- Spreadsheet Test Client/NetworkController/Networking.cs
+++ Spreadsheet Test Client/NetworkController/Networking.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace NetworkUtil
+namespace TestHandler
 {
     /// <summary>
     /// This class encapsulates code required for connecting clients and server via TcpListener.
--- Spreadsheet Test Client/Spreadsheet Test Client/TestHandler.cs
+++ Spreadsheet Test Client/Spreadsheet Test Client/TestHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 
 namespace TestHandler
 {
@@ -6,7 +7,7 @@ namespace TestHandler
     {
         private static int numTests = 1;
 
-        private static ClientHandler c;
+        private static string IP;
 
         static void Main(string[] args)
         {           
@@ -14,27 +15,44 @@ namespace TestHandler
                 Console.WriteLine(numTests.ToString());
             else
             {
-                c = new ClientHandler(args[1]);    // Second main Arg is IP
+                IP = args[1];    // Second main Arg is IP
 
-                if (args[1] == "1")                // First main arg is Test number
+                if (args[0] == "0")                // First main arg is Test number
+                {  Test0();  }
+                else if (args[0] == "1")
                 { /*run test 1*/ }
             }
         }
 
-        static void Test1 ()
+        private static void Timeout (Object source, ElapsedEventArgs e)
+        {  Console.WriteLine("Test Failed : Timeout");  }
+
+        static void Test0 ()
         {
-            Console.WriteLine("Max runtime");
+            Console.WriteLine("Max runtime: 4 seconds");
 
-            Console.WriteLine("Test Name");
+            Console.WriteLine("Basic Connection Test");
 
-            /*
-            *    RUN THE TEST YAAAAAAY
-            */
+            GhostClient client1 = new GhostClient(IP);
 
-            if (true /*test passes*/)
-                Console.WriteLine("Test Passed");
-            else
-                Console.WriteLine("Test Failed");
+            Timer time = new Timer(4000);
+            time.Elapsed += Timeout;
+
+            time.Start();
+            client1.Connect();
+
+            while (time.Enabled)
+            {
+                if (client1.HasReceivedSpreadsheets())
+                {
+                    Console.WriteLine("Test Passed");
+                    time.Stop();
+                    time.Close();
+                    return;
+                }
+            }
+
+            time.Close();
         }
     }
 }
